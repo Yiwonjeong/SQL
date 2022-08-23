a3b406f0cbd4f1d8e22a2b37121705e1706a04c2
Yiwonjeong <near_and_dear_studio@naver.com>
2022-08-23 19:43:23 +0900

    2022/08/23 수업 업로드

--- 제1장 SQL 기초.sql
+++ 제1장 SQL 기초.sql
@@ -14,10 +14,9 @@ create table `User1`(
 	`age` int
 );
 
-drop table `User1`
+drop table `User1`;
 
- #실습 1-3
-
+#실습 1-3
 insert into `User1` values ('A101','김유신','010-1234-1111',25);
 insert into `User1` values ('A102','김춘추','010-1234-2222',23);
 insert into `User1` values ('A103','장보고','010-1234-3333',32);
@@ -48,5 +47,51 @@ delete from `User1` where `uid`='A102' and `age`='25';
 delete from `User1` where `age` >= 30;
 
 #실습 1-7
+create table `TblUser`(
+	`userID` varchar(10),
+    `userName` varchar(10),
+    `userHp` char(13),
+    `userAge` tinyint,
+    `userAddr` varchar(20)
+);
+create table `TblProduct`(
+`proCode` int,
+`prdName` varchar(10),
+`prdPrice` int,
+`prdAmount` int,
+`prdCompany` varchar(10),
+`prdMakeDate` date
+);
+
+
 #실습 1-8
+insert into `TblUser` values ('p101','김유신','010-1234-1001','25','서울시 중구');
+insert into `TblUser` values ('p102','김춘추','010-1234-1002','23','부산시 금정구');
+insert into `TblUser` values ('p103','장보고',null,'31','경기도 광주');
+insert into `TblUser` values ('p104','강감찬',null,null,'경남 창원시');
+insert into `TblUser` values ('p105','이순신','010-1234-1005','50',null);
+
+insert into `TblProduct` values ('1','냉장고','800','10','LG','2022-01-06');
+insert into `TblProduct` values ('2','노트북','1,200','20','삼성','2022-01-06');
+insert into `TblProduct` values ('3','TV','1,400','6','LG','2022-01-06');
+insert into `TblProduct` values ('4','세탁기','1,000','8','LG','2022-01-06');
+insert into `TblProduct` values ('5','컴퓨터','1,100','0',null,null);
+insert into `TblProduct` values ('6','휴대폰','900','102','삼성','2022-01-06');
+
 #실습 1-9
+select * from `TblUser`;
+select `UserName` from `TblUser`;
+select `userName`,`userHp` from `TblUser`
+select * from `TblUser` where `userID`='p102';
+select * from `TblUser` where `userID`='p104' or 'userID'='p105';
+select * from `TblUser` where `userID`='';
+select * from `TblUser` where `userID`='';
+select * from `TblUser` where `userID`='';
+select * from `TblUser` where `userID`='';
+
+
+
+
+
+
+
